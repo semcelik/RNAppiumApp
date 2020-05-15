4f8e6111f10f22235b866c543bef5c894c633093
Semih Çelik <semih.celik@iyzico.com>
2020-05-15 18:10:01 +0300

    Refactor App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, View, Text } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
