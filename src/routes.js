7875938950be797da957bbfd5a7ec0dc9858f091
Semih Çelik <semih.celik@iyzico.com>
2020-05-15 18:37:14 +0300

    Add navigation and screens

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import HomeScreen from './screens/HomeScreen';
+import FirstScreen from './screens/FirstScreen';
+import SecondScreen from './screens/SecondScreen';
+import ThirdScreen from './screens/ThirdScreen';
+
+const ROUTES = [
+  {
+    name: 'HomeScreen',
+    component: HomeScreen,
+  },
+  {
+    name: 'FirstScreen',
+    component: FirstScreen,
+  },
+  {
+    name: 'SecondScreen',
+    component: SecondScreen,
+  },
+  {
+    name: 'ThirdScreen',
+    component: ThirdScreen,
+  },
+];
+
+export default ROUTES;
