87119ef5ad085523f8b446a78cdd48e245ebce2f
Semih Çelik <semih.celik@iyzico.com>
2020-06-02 15:33:29 +0300

    x

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "##############################"
+echo "PRE_BUILD script"
+echo "APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY"
+echo "APPCENTER_OUTPUT_DIRECTORY: $APPCENTER_OUTPUT_DIRECTORY"
+echo "APPCENTER_REACTNATIVE_PACKAGE: $APPCENTER_REACTNATIVE_PACKAGE"
+echo "PLATFORM: $PLATFORM"
+
+
+
+echo "###"
+ls $APPCENTER_SOURCE_DIRECTORY
+echo "###"
+ls $APPCENTER_OUTPUT_DIRECTORY
+
+echo "##############################"
