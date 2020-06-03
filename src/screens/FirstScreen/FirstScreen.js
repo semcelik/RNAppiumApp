11890ff35f51df04bf5aee963c2967ea8e808042
Semih Celik <s.celikce@gmail.com>
2020-06-04 02:43:51 +0300

    test

--- src/screens/FirstScreen/FirstScreen.js
+++ src/screens/FirstScreen/FirstScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 function FirstScreen() {
   return (
     <View style={styles.container}>
-      <Text>FirstScreen</Text>
+      <Text>FirstScreexn</Text>
     </View>
   );
 }
