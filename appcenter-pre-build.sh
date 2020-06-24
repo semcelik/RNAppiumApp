60a703ccfceaa5f0c37820aaf8801f6f9295b3c7
Semih Celik <s.celikce@gmail.com>
2020-06-24 11:58:17 +0300

    Add appcenter-post-build.sh (#8)
    * first
    
    * d2

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "##############################"
+echo "PRE_BUILD script"
+echo "APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY"
+echo "APPCENTER_OUTPUT_DIRECTORY: $APPCENTER_OUTPUT_DIRECTORY"
+echo "PLATFORM: $PLATFORM"
+echo "APP_TOKEN: $APP_TOKEN"
+
+echo "###"
+ls $APPCENTER_SOURCE_DIRECTORY
+echo "###"
+ls $APPCENTER_OUTPUT_DIRECTORY
+
+echo "##############################"
