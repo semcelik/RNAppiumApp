25002140a2ce6c622c74c01823cf81b8f60ee670
Semih Çelik <semih.celik@iyzico.com>
2020-06-26 12:32:37 +0300

    d

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -18,7 +18,7 @@ if [[ $TEST_PLATFORM != "android" && $TEST_PLATFORM != "ios" ]]; then
   exit 0
   fi
 
-git clone --single-branch https://github.com/semcelik/appium-java-playground.git
+git clone -b b-5 --single-branch https://github.com/semcelik/appium-java-playground.git
 
 E2E_PATH=appium-java-playground
 TEST_SOURCE=$APPCENTER_SOURCE_DIRECTORY/$E2E_PATH/target/upload
@@ -27,7 +27,7 @@ cd $E2E_PATH
 sh generate-test-sources.sh $TEST_PLATFORM
 cd ..
 
-appcenter test run appium --app $TEST_APP --devices $TEST_DEVICES --app-path $APPCENTER_OUTPUT_DIRECTORY/$TEST_APP_PATH --test-series $TEST_SERIES --locale $TEST_LOCALE --build-dir $TEST_SOURCE --token $APP_TOKEN --async
+appcenter test run appium --app $TEST_APP --devices $TEST_DEVICES --app-path $APPCENTER_OUTPUT_DIRECTORY/$TEST_APP_PATH --test-series $TEST_SERIES --locale $TEST_LOCALE --build-dir $TEST_SOURCE --test-parameter "app_env=TEST_PLATFORM=$TEST_PLATFORM" --token $APP_TOKEN --async
 
 echo "##############################TESTING FINISHED"
 
