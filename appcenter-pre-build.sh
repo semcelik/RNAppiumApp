2cc00ca2b010efea3ca21e27ebbd8ba816ab1803
Semih Celik <s.celikce@gmail.com>
2020-06-26 10:16:51 +0300

    Change appcenter config (#9)

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,14 +2,5 @@
 
 echo "##############################"
 echo "PRE_BUILD script"
-echo "APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY"
-echo "APPCENTER_OUTPUT_DIRECTORY: $APPCENTER_OUTPUT_DIRECTORY"
-echo "PLATFORM: $PLATFORM"
-echo "APP_TOKEN: $APP_TOKEN"
-
-echo "###"
-ls $APPCENTER_SOURCE_DIRECTORY
-echo "###"
-ls $APPCENTER_OUTPUT_DIRECTORY
 
 echo "##############################"
