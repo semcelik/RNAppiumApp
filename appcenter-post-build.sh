57c47de35450ae5a229cff6a88ab42ccfdf244ad
Semih Çelik <semih.celik@iyzico.com>
2020-06-26 09:58:52 +0300

    Change appcenter config

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,26 +1,46 @@
 #!/usr/bin/env bash
 
 echo "##############################POST BUILD TESTING STARTED"
-echo "APPCENTER_BUILD_ID: $APPCENTER_BUILD_ID"
-echo "APPCENTER_BRANCH: $APPCENTER_BRANCH"
-echo "APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY"
-echo "APPCENTER_OUTPUT_DIRECTORY: $APPCENTER_OUTPUT_DIRECTORY"
-echo "APPCENTER_TRIGGER: $APPCENTER_TRIGGER"
+echo "APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY" #/Users/runner/runners/2.170.1/work/1/s
+echo "APPCENTER_OUTPUT_DIRECTORY: $APPCENTER_OUTPUT_DIRECTORY" #/Users/runner/runners/2.170.1/work/1/a/build
+ls $APPCENTER_OUTPUT_DIRECTORY
 
-git clone https://github.com/semcelik/appium-java-playground.git
+echo "TEST_PLATFORM: $TEST_PLATFORM"
+
+if [ $TEST_PLATFORM == "android" ];
+then
+  APP="s.celikk08-gmail.com/rnandroid"
+  DEVICES="b4bd15f4"
+  TEST_SERIES="master"
+  APP_PATH=$APPCENTER_OUTPUT_DIRECTORY/*.apk
+elif [ $TEST_PLATFORM == "ios" ]
+then
+  APP="s.celikk08-gmail.com/rnios"
+  DEVICES="s.celikk08-gmail.com/test"
+  TEST_SERIES="master"
+  APP_PATH=$APPCENTER_OUTPUT_DIRECTORY/*.ipa # need to build with app sign for this
+else
+  echo "PLATFORM not found. it should be 'ios' or 'android'"
+  exit 0
+fi
+
+git clone --single-branch https://github.com/semcelik/appium-java-playground.git
 
 E2E_PATH=appium-java-playground
 TEST_SOURCE=$APPCENTER_SOURCE_DIRECTORY/$E2E_PATH/target/upload
 
+
+echo "APP: $APP"
+echo "DEVICES: $DEVICES"
+echo "TEST_SERIES: $TEST_SERIES"
+echo "APP_PATH: $APP_PATH"
+
+
 cd $E2E_PATH
-sh generate-test-sources.sh
+sh generate-test-sources.sh $TEST_PLATFORM
 cd ..
 
-echo "###"
-ls $APPCENTER_SOURCE_DIRECTORY
-echo "###"
-ls $APPCENTER_OUTPUT_DIRECTORY
+appcenter test run appium --app $APP --devices $DEVICES --app-path $APP_PATH --test-series $TEST_SERIES --locale "en_US" --build-dir $TEST_SOURCE --token $APP_TOKEN --async
 
-appcenter test run appium --app "semcelik/RNAppiumApp" --devices "semcelik/test-1" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "launch-tests" --locale "en_US" --build-dir $TEST_SOURCE --token $APP_TOKEN --async
 echo "##############################TESTING FINISHED"
 
