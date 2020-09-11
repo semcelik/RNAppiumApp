c02d24f672470f2de4ef5b823eb51c04dfee2051
Semih Celik <s.celikce@gmail.com>
2020-09-11 19:51:28 +0300

    Update SecondScreen.test.js

--- src/screens/SecondScreen/SecondScreen.test.js
+++ src/screens/SecondScreen/SecondScreen.test.js
@@ -15,7 +15,7 @@ describe('SecondScreen', () => {
     const { getByA11yLabel } = render(
       <SecondScreen navigation={navigationProps} />,
     );
-    const thirdScreenButton = getByA11yLabel('thirdButton');
+    const thirdScreenButton = getByA11yLabel('thirdButtonx');
 
     expect(navigationProps.navigate).toBeCalledTimes(0);
 
